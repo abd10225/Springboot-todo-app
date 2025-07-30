f19e8723ea072cfd1c162dc049ec53711523d87f
abd10225 <abdullahnasar0513@gmail.com>
2025-07-31 03:08:51 +0530

    to do app configurations done an =d my sql database connected successfully

---
+++ src/main/java/controller/TaskController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import models.Task;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import services.TaskService;
+
+import java.util.List;
+
+@Controller
+//@RequestMapping("/tasks")
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public String getTask(Model model){
+        List<Task> tasks = taskService.getAllTasks();
+        model.addAttribute("tasks", tasks);
+        return "tasks";;
+    }
+}
---
+++ src/main/java/services/TaskService.java
@@ -0,0 +1,21 @@
+package services;
+
+import models.Task;
+import org.springframework.stereotype.Service;
+import repository.TaskRepository;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+}
